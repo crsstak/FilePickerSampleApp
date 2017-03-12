1c183127786947b797905ced6f496211c5e4db98
高橋 辰伍 <takahashi@be-org.co.jp>
2017-03-12 10:54:03 +0900

    プロジェクト ファイルを追加します。

---
+++ FilePickerSampleApp/FilePickerSampleApp/Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Plugin.FilePicker;
+using Plugin.FilePicker.Abstractions;
+using System;
+using Xamarin.Forms;
+
+namespace FilePickerSampleApp.Views
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private async void Button_Clicked(object sender, EventArgs e)
+		{
+			FileData file = await CrossFilePicker.Current.PickFile();
+			lblFileName.Text = file.FileName;
+		}
+	}
+}
